cfbf5d9efd3067c7bc562d911c0a94c2791ff82c
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2022-06-08 01:26:51 +0600

    feat: Day 4

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 
-function Categories() {
-    const [activeIndex, setActiveIndex] = useState(0);
-
+function Categories({ value, onClickCategory }) {
     const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
     return (
         <div className="categories">
             <ul>
                 {categories.map((item, index) => (
-                    <li key={index} onClick={() => setActiveIndex(index)} className={activeIndex === index ? 'active' : ''}>
+                    <li key={index} onClick={() => onClickCategory(index)} className={value === index ? 'active' : ''}>
                         {item}
                     </li>
                 ))}
