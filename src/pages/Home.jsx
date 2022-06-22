713041ee3b0c2be7fbae27417964b64c4c20413e
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2022-06-23 00:59:13 +0600

    feat: remove props drilling and add useContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { SearchContext } from '../App';
 import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
 
-function Home({ searchValue }) {
+function Home() {
+    const { searchValue } = React.useContext(SearchContext);
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [categoryId, setCategoryId] = useState(0);
@@ -18,7 +20,7 @@ function Home({ searchValue }) {
 
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
-    const caregoryId = categoryId > 0 ? `category=${categoryId}` : '';
+    const caregoryId = categoryId > 0 ? `&category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
     useEffect(() => {
