f579219378f0c7114cd0425b8911684a2be2e844
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2022-07-21 16:34:55 +0600

    feat: Day 9, 10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import logo from '../assets/img/pizza-logo.svg';
 import Search from './Search';
 
 function Header() {
+    const { items, totalPrice } = useSelector(state => state.cart);
+    const countItems = items.reduce((acc, item) => {
+        return item.count + acc;
+    }, 0);
+
     return (
         <div className="header">
             <div className="container">
                 <Link to="/">
                     <div className="header__logo">
                         <img width="38" src={logo} alt="Pizza logo" />
-
                         <div>
                             <h1>React Pizza v2</h1>
                             <p>самая вкусная пицца во вселенной</p>
@@ -19,7 +24,7 @@ function Header() {
                 <Search />
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
-                        <span>520 ₽</span>
+                        <span>{totalPrice} ₽</span>
                         <div className="button__delimiter"></div>
                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
@@ -44,7 +49,7 @@ function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>3</span>
+                        <span>{countItems}</span>
                     </Link>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 import { SearchContext } from '../App';
 import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
-import Sort from '../components/Sort';
+import Sort, { sortList } from '../components/Sort';
 
 function Home() {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    const isSearch = useRef(false);
+    const isMounted = useRef(false);
+
     const { categoryId, sort, currentPage } = useSelector(state => state.filter);
 
     const { searchValue } = React.useContext(SearchContext);
@@ -27,7 +34,7 @@ function Home() {
         dispatch(setCurrentPage(number));
     };
 
-    useEffect(() => {
+    const fetchPizzas = () => {
         setIsLoading(true);
 
         const sortBy = sort.sortProperty.replace('-', '');
@@ -43,8 +50,47 @@ function Home() {
                 setItems(res.data);
                 setIsLoading(false);
             });
+    };
 
+    // Если изменили параметры и был первый рендер
+    useEffect(() => {
+        if (isMounted.current) {
+            const queryString = qs.stringify({
+                sortProperty: sort.sortProperty,
+                categoryId,
+                currentPage,
+            });
+
+            navigate(`?${queryString}`);
+        }
+        isMounted.current = true;
+    }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+    // Если был первый рендер, то проверяем и сохранаем в редаксе
+    useEffect(() => {
+        if (window.location.search) {
+            const params = qs.parse(window.location.search.substring(1));
+
+            const sort = sortList.find(obj => obj.sortProperty === params.sortProperty);
+
+            dispatch(
+                setFilters({
+                    ...params,
+                    sort,
+                })
+            );
+            isSearch.current = true;
+        }
+    }, []);
+
+    // Если был первый рендер, то запрашиваем пиццы
+    useEffect(() => {
         window.scrollTo(0, 0);
+        if (!isSearch.current) {
+            fetchPizzas();
+        }
+
+        isSearch.current = false;
     }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
     const pizzas = items.map(item => <PizzaBlock key={item.id} {...item} />);
