1d2fb54b8bd249b55fecc577a922a7bfcc55612f
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2022-12-19 19:54:09 +0600

    Урок #18

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setSort } from '../redux/slices/filterSlice';
+import { selectSort, setSort } from '../redux/slices/filterSlice';
 
 export const sortList = [
     { name: 'популярности (DESC)', sortProperty: 'rating' },
@@ -13,7 +13,7 @@ export const sortList = [
 
 function Sort() {
     const dispatch = useDispatch();
-    const sort = useSelector(state => state.filter.sort);
+    const sort = useSelector(selectSort);
     const sortRef = useRef();
 
     const [open, setOpen] = useState(false);
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -43,6 +43,8 @@ const cartSlice = createSlice({
         },
     },
 });
+export const selectCartItemById = id => state => state.cart.items.find(obj => obj.id === id);
+export const selectCart = state => state.cart;
 
 export const { addItem, removeItem, clearItem, minusItem } = cartSlice.actions;
 
