162be9955359f11e727b933fde7bb678cafc245a
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2022-12-25 20:52:18 +0600

    Урок #24, #25

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,31 @@
 import React, { useRef } from 'react';
 import qs from 'qs';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 import { selectFilter, setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
-import { fetchPizzas, selectPizzaData } from '../redux/slices/pizzaSlice';
+import { fetchPizzas, SearchPizzaParams, selectPizzaData } from '../redux/slices/pizzaSlice';
 import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
-import Sort, { sortList } from '../components/Sort';
+import SortPopup, { sortList } from '../components/Sort';
+import { useAppDispatch } from '../redux/store';
 
 const Home: React.FC = () => {
     const navigate = useNavigate();
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const isSearch = useRef(false);
     const isMounted = useRef(false);
 
     const { categoryId, sort, currentPage, searchValue } = useSelector(selectFilter);
     const { items, status } = useSelector(selectPizzaData);
 
-    const onChangeCategory = (index: number) => {
+    const onChangeCategory = React.useCallback((index: number) => {
         dispatch(setCategoryId(index));
-    };
+    }, []);
 
     const onChangePage = (page: number) => {
         dispatch(setCurrentPage(page));
@@ -37,13 +38,12 @@ const Home: React.FC = () => {
         const search = searchValue ? `&search=${searchValue}` : '';
 
         dispatch(
-            //@ts-ignore
             fetchPizzas({
                 sortBy,
                 order,
                 category,
                 search,
-                currentPage,
+                currentPage: String(currentPage),
             })
         );
 
@@ -53,28 +53,32 @@ const Home: React.FC = () => {
     // Если изменили параметры и был первый рендер
     useEffect(() => {
         if (isMounted.current) {
-            const queryString = qs.stringify({
+            const params = {
+                categoryId: categoryId > 0 ? categoryId : null,
                 sortProperty: sort.sortProperty,
-                categoryId,
                 currentPage,
-            });
+            };
+            const queryString = qs.stringify(params, { skipNulls: true });
 
-            navigate(`?${queryString}`);
+            navigate(`/?${queryString}`);
         }
-        isMounted.current = true;
+        if (!window.location.search) {
+            dispatch(fetchPizzas({} as SearchPizzaParams));
+        }
     }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
     // Если был первый рендер, то проверяем и сохранаем в редаксе
     useEffect(() => {
         if (window.location.search) {
-            const params = qs.parse(window.location.search.substring(1));
-
-            const sort = sortList.find(obj => obj.sortProperty === params.sortProperty);
+            const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
+            const sort = sortList.find(obj => obj.sortProperty === params.sortBy);
 
             dispatch(
                 setFilters({
-                    ...params,
-                    sort,
+                    searchValue: params.search,
+                    categoryId: Number(params.category),
+                    currentPage: Number(params.currentPage),
+                    sort: sort || sortList[0],
                 })
             );
             isSearch.current = true;
@@ -83,26 +87,17 @@ const Home: React.FC = () => {
 
     // Если был первый рендер, то запрашиваем пиццы
     useEffect(() => {
-        window.scrollTo(0, 0);
-        // if (!isSearch.current) {
         getPizzas();
-        // }
-
-        isSearch.current = false;
     }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-    const pizzas = items.map((item: any) => (
-        <Link key={item.id} to={`/pizza/${item.id}`}>
-            <PizzaBlock {...item} />
-        </Link>
-    ));
+    const pizzas = items.map((item, index) => <PizzaBlock key={index} {...item} />);
     const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
     return (
         <div className="container">
             <div className="content__top">
                 <Categories value={categoryId} onChangeCategory={(index: number) => onChangeCategory(index)} />
-                <Sort />
+                <SortPopup />
             </div>
             <h2 className="content__title">Все пиццы</h2>
             {status === 'error' ? (
--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,6 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
 
-const initialState = {
+export type CartItem = {
+    id: string;
+    title: string;
+    price: number;
+    imageUrl: string;
+    type: string;
+    size: number;
+    count: number;
+};
+
+interface CartSliceState {
+    totalPrice: number;
+    items: CartItem[];
+}
+
+const initialState: CartSliceState = {
     totalPrice: 0,
     items: [],
 };
@@ -9,7 +25,7 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addItem(state, action) {
+        addItem(state, action: PayloadAction<CartItem>) {
             const findItem = state.items.find(obj => obj.id === action.payload.id);
 
             if (findItem) {
@@ -25,17 +41,28 @@ const cartSlice = createSlice({
                 return acc + item.price * item.count;
             }, 0);
         },
-        minusItem(state, action) {
+        minusItem(state, action: PayloadAction<string>) {
             const findItem = state.items.find(obj => obj.id === action.payload);
+
             if (findItem) {
                 findItem.count--;
             }
+
             state.totalPrice = state.items.reduce((acc, item) => {
                 return acc + item.price * item.count;
             }, 0);
         },
-        removeItem(state, action) {
+        removeItem(state, action: PayloadAction<string>) {
             state.items = state.items.filter(obj => obj.id !== action.payload);
+
+            // state.totalPrice -= state.items.reduce((acc, item) => {
+            //     return acc + item.price * item.count;
+            // }, 0);
+            // console.log();
+
+            if (!state.items.length) {
+                state.totalPrice = 0;
+            }
         },
         clearItem(state) {
             state.items = [];
@@ -43,8 +70,9 @@ const cartSlice = createSlice({
         },
     },
 });
-export const selectCartItemById = id => state => state.cart.items.find(obj => obj.id === id);
-export const selectCart = state => state.cart;
+
+export const selectCart = (state: RootState) => state.cart;
+export const selectCartItemById = (id: string) => (state: RootState) => state.cart.items.find(obj => obj.id === id);
 
 export const { addItem, removeItem, clearItem, minusItem } = cartSlice.actions;
 
