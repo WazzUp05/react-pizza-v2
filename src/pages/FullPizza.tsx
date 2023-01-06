d41af92a88e90f0958802ee0b35c84fff38fafa6
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-01-06 20:25:44 +0600

    Оптимизируем сборку JS-файла. Code Splitting и тд.

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const FullPizza: React.FC = () => {
     const [pizza, setPizza] = React.useState<{
@@ -30,8 +30,20 @@ const FullPizza: React.FC = () => {
                 <>
                     <img src={pizza.imageUrl} alt="" />
                     <h2>{pizza.title}</h2>
-
                     <b>{pizza.price}</b>
+                    <br />
+                    <Link to="/" className="button button--outline button--add go-back-btn">
+                        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7 13L1 6.93015L6.86175 1"
+                                stroke="#D3D3D3"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>
+                        </svg>
+                        <span>Вернуться назад</span>
+                    </Link>
                 </>
             )}
         </div>
