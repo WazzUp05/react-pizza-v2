cf4ac1df93ae69334be48cbafdafb1b7298c5476
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2023-01-10 19:15:50 +0600

    fix yml

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
 import { addItem, removeItem, minusItem, CartItem } from '../redux/slices/cartSlice';
 
